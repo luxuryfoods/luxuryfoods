99ed3bd48a76dcfc569004ce97a6539aa5157b70
luxuryfoods <mktluxuryfoods@gmail.com>
2024-11-23 14:16:47 +0700

    Update base.js

--- assets/base.js
+++ assets/base.js
@@ -16,7 +16,14 @@ $(document).ready(function() {
                     $('<div>', {
                         class: 'contact-now gst-p-border-color gst-p-background-color--hover text-light--hover svg-light--hover',
                         'rv-on-click': 'methods.onClickBuyNow | args product'
-                    }).append($('<span>').text('Liên hệ ngay'))
+					}).append(
+						$('<img>', {
+							src: 'https://luxuryfoods.github.io/luxuryfoods/assets/phone-call.svg',
+							alt: 'Liên hệ ngay',
+							class: 'contact-icon'
+						}),
+						$('<span>').text('Liên hệ ngay')
+					);
                 )
             );
 
