e4318b555e42bec260da284534c8bbcada6aa2e6
luxuryfoods <mktluxuryfoods@gmail.com>
2024-11-23 14:09:28 +0700

    Update base.js

--- assets/base.js
+++ assets/base.js
@@ -11,10 +11,11 @@ $(document).ready(function() {
         });
 
         if (buttons.length > 0) {
-			const newContent = $('<div>', { class: 'w-100' }).append(
-				$('<div>', { class: 'd-flex justify-content-around mb-2' }).append(
-					$('<div>', {
-						class: 'buy-now w-100 contact-now gst-p-border-color gst-p-background-color--hover text-light--hover svg-light--hover'
+            const newContent = $('<div>', { class: 'w-100' }).append(
+                $('<div>', { class: 'd-flex justify-content-around mb-2' }).append(
+                    $('<div>', {
+                        class: 'buy-now w-100 contact-now gst-p-border-color gst-p-background-color--hover text-light--hover svg-light--hover',
+                        'rv-on-click': 'methods.onClickBuyNow | args product'
 					}).append(
 						$('<img>', {
 							src: 'https://luxuryfoods.github.io/luxuryfoods/assets/phone-call.svg',
